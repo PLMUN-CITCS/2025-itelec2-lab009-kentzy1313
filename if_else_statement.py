8a1c0b244b827ebb80418df0b4a9292a2ce1c3ca
unknown <SebastianTyron_bsit@plmun.edu.ph>
2025-02-15 12:16:48 +0800

    Submitting Python Week 04 - Laboratory # 09

---
+++ if_else_statement.py
@@ -0,0 +1,22 @@
+#TYRON SEBASTIAN 
+#LABORATORY009
+
+def main():
+   user_input = input("Enter a number: ")
+   number = int(user_input)
+
+   if number % 2 == 0:
+    print("The number", number, "is Even.")
+
+else:
+    print("The number", number, "is Odd.")
+    
+try:
+    #code here
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    main()
+    
+
